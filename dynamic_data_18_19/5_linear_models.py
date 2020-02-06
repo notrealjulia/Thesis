c972f511055b6c3d950f94d4fecc2f82583f4f9e
Maybe real Julia <julia.spicina@gmail.com>
2020-02-06 15:13:30 +0100

    preformed linear regression to find length of ships with simplified dynamic data

---
+++ dynamic_data_18_19/5_linear_models.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 14:46:31 2020
+
+@author: JULSP
+"""
+
+
+import pandas as pd
+from os.path import dirname
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats.mstats import mode
+import seaborn as sn
+from sklearn.linear_model import LinearRegression, Lasso, BayesianRidge, HuberRegressor
+from sklearn.metrics import mean_squared_error 
+from sklearn.model_selection import train_test_split 
+from sklearn import preprocessing
+from sklearn import metrics
+
+path = dirname(__file__)
+data = pd.read_csv(path + "/dynamic_for_linear_models.csv")
+
+def evaluation(model, predicted, actual):
+    errors = abs(predicted-actual)
+    averageError = np.mean(abs(predicted-actual))
+    mape = 100 * (errors / actual)
+    accuracy = 100-np.mean(mape)
+    mse = metrics.mean_squared_error(predicted, actual)
+    print(np.mean(mape))
+    print(model, '\nAverage Absolute Error = ', round(averageError),'m', '\nMSE = ', round(mse, 2), '\nAccuracy = ', round(accuracy, 2), '%')
+    return({'averageAbsoluteError': averageError, 'mse':mse, 'accuracy':accuracy})
+
+#%%
+    
+"""
+Linear Regression
+"""
+
+def LR(output,features, labels):
+    X = features.values#.reshape(-1,1) # features
+    y = labels.values#.reshape(-1,1) # labels
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    regressor = LinearRegression()  
+    regressor.fit(X_train, y_train) #training the algorithm
+    y_pred = regressor.predict(X_test) #predicting
+
+    # visualiation of 10 vessels (predicted vs actual value)
+    lr = np.round(pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()}))
+    lr = lr.head(10)
+    lr.plot(kind='bar',figsize=(10,6))
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    plt.title('Actual vs Predicted {0} (10 vessels), \n Linear Regression'.format(output), fontsize=12)
+    plt.show()
+    #evaluation metrics
+    lrEvaluation = evaluation('Linear Regression {0}'.format(output), y_pred, y_test)
+    return(lrEvaluation, lr)
+
+#Two most correlated features
+speed_and_rot = data[['mean_speed', 'ROT_min']] #accuracy = 71.4 %
+lrLength = LR('Length',speed_and_rot, data['length_from_data_set'])
+
+speed_and_rot_all = data[['mean_speed', 'top_speed', 'std_speed', 'Speed_mode', 'Speed_median', 'ROT_mean', 'ROT_max', 'ROT_median', 'ROT_min']]
+lrLengthall = LR('Length',speed_and_rot_all, data['length_from_data_set']) #accuracy = 72.75 %
+
+#%%
+"""
+Lasso Regression
+"""
+def LassoRegression(output,features, labels):
+    X = features.values#.reshape(-1,1) # features
+    y = labels.values#.reshape(-1,1) # labels
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    regressor = Lasso()  
+    regressor.fit(X_train, y_train) #training the algorithm
+    y_pred = regressor.predict(X_test) #predicting
+
+    # visualiation of 10 vessels (predicted vs actual value)
+    lr = np.round(pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()}))
+    lr = lr.head(10)
+    lr.plot(kind='bar',figsize=(10,6))
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    plt.title('Actual vs Predicted {0} (10 vessels), \n Lasso Regression'.format(output), fontsize=12)
+    plt.show()
+    #evaluation metrics
+    lrEvaluation = evaluation('Lasso Regression {0}'.format(output), y_pred, y_test)
+    return(lrEvaluation, lr)
+
+
+#Two most correlated features
+speed_and_rot = data[['mean_speed', 'ROT_min']] #accuracy = 71.38 %
+Lasso_Length = LassoRegression('Length',speed_and_rot, data['length_from_data_set'])
+
+speed_and_rot_all = data[['mean_speed', 'top_speed', 'std_speed', 'Speed_mode', 'Speed_median', 'ROT_mean', 'ROT_max', 'ROT_median', 'ROT_min']]
+Lasso_Length_all = LassoRegression('Length',speed_and_rot_all, data['length_from_data_set']) #accuracy = 72.77 %
+
+#%%
+"""
+Bayesian Ridge
+"""
+def BayesianRidgeRegression(output,features, labels):
+    X = features.values#.reshape(-1,1) # features
+    y = labels.values#.reshape(-1,1) # labels
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    regressor = BayesianRidge()  
+    regressor.fit(X_train, y_train) #training the algorithm
+    y_pred = regressor.predict(X_test) #predicting
+
+    # visualiation of 10 vessels (predicted vs actual value)
+    lr = np.round(pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()}))
+    lr = lr.head(10)
+    lr.plot(kind='bar',figsize=(10,6))
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    plt.title('Actual vs Predicted {0} (10 vessels), \n BayesianRidge Regression'.format(output), fontsize=12)
+    plt.show()
+    #evaluation metrics
+    lrEvaluation = evaluation('BayesianRidge Regression {0}'.format(output), y_pred, y_test)
+    return(lrEvaluation, lr)
+
+#Two most correlated features
+speed_and_rot = data[['mean_speed', 'ROT_min']] #accuracy = 71.39 %
+Lasso_Length = BayesianRidgeRegression('Length',speed_and_rot, data['length_from_data_set'])
+
+speed_and_rot_all = data[['mean_speed', 'top_speed', 'std_speed', 'Speed_mode', 'Speed_median', 'ROT_mean', 'ROT_max', 'ROT_median', 'ROT_min']]
+Lasso_Length_all = BayesianRidgeRegression('Length',speed_and_rot_all, data['length_from_data_set']) #accuracy = 72.77 %
+
+#%%
+
+"""
+HuberRegressor - should be better for datasets with strong outliers
+"""
+def HuberReg(output,features, labels):
+    X = features.values#.reshape(-1,1) # features
+    y = labels.values#.reshape(-1,1) # labels
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    regressor = HuberRegressor()  
+    regressor.fit(X_train, y_train) #training the algorithm
+    y_pred = regressor.predict(X_test) #predicting
+
+    # visualiation of 10 vessels (predicted vs actual value)
+    lr = np.round(pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()}))
+    lr = lr.head(10)
+    lr.plot(kind='bar',figsize=(10,6))
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    plt.title('Actual vs Predicted {0} (10 vessels), \n Huber Regressor'.format(output), fontsize=12)
+    plt.show()
+    #evaluation metrics
+    lrEvaluation = evaluation('Huber Regressor {0}'.format(output), y_pred, y_test)
+    return(lrEvaluation, lr)
+
+#Two most correlated features
+speed_and_rot = data[['mean_speed', 'ROT_min']] #accuracy = 72.79 %
+Lasso_Length = HuberReg('Length',speed_and_rot, data['length_from_data_set'])
+
+speed_and_rot_all = data[['mean_speed', 'top_speed', 'std_speed', 'Speed_mode', 'Speed_median', 'ROT_mean', 'ROT_max', 'ROT_median', 'ROT_min']]
+Lasso_Length_all = HuberReg('Length',speed_and_rot_all, data['length_from_data_set']) #accuracy = 73.83 %
+
+#%%
+
+"""
+DecisionTreeRegressor - THE BEST
+"""
+
+from sklearn.tree import DecisionTreeRegressor
+
+def TreesReg(output,features, labels):
+    X = features.values#.reshape(-1,1) # features
+    y = labels.values#.reshape(-1,1) # labels
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    regressor = DecisionTreeRegressor()  
+    regressor.fit(X_train, y_train) #training the algorithm
+    y_pred = regressor.predict(X_test) #predicting
+
+    # visualiation of 10 vessels (predicted vs actual value)
+    lr = np.round(pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()}))
+    lr = lr.head(10)
+    lr.plot(kind='bar',figsize=(10,6))
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    plt.title('Actual vs Predicted {0} (10 vessels), \n Decision Tree Regressor'.format(output), fontsize=12)
+    plt.show()
+    #evaluation metrics
+    lrEvaluation = evaluation('Decision Tree Regressor {0}'.format(output), y_pred, y_test)
+    return(lrEvaluation, lr)
+
+#Two most correlated features
+speed_and_rot = data[['mean_speed', 'ROT_min']] #accuracy = 75.1 %
+Lasso_Length = TreesReg('Length',speed_and_rot, data['length_from_data_set'])
+
+speed_and_rot_all = data[['mean_speed', 'top_speed', 'std_speed', 'Speed_mode', 'Speed_median', 'ROT_mean', 'ROT_max', 'ROT_median', 'ROT_min']]
+Lasso_Length_all = TreesReg('Length',speed_and_rot_all, data['length_from_data_set']) #accuracy = 79.97 %
--- dynamic_data_18_19/static_with_mode_dynamic_values_2.py
+++ dynamic_data_18_19/static_with_mode_dynamic_values_2.py
@@ -20,10 +20,9 @@ static_data = pd.read_csv(path + "/static_with_ROT_Speed_1464.csv")
 example_df =pd.read_csv(path + '/245176000.csv', sep='	', index_col=None, error_bad_lines=False)
 
 #static_data = static_data[:5]
-example_df['speed_rounded'] = np.nan
-example_df['speed_rounded'] = example_df.iloc[:,3].round(0)
-
-mode_of_mean_speed = example_df.mode()['speed_rounded'][0]
+#example_df['speed_rounded'] = np.nan
+#example_df['speed_rounded'] = example_df.iloc[:,3].round(0)
+#mode_of_mean_speed = example_df.mode()['speed_rounded'][0]
 
 #%%
 
@@ -33,7 +32,9 @@ Getting mode speed and ROT
 #For ROT drop 0 values
 static_data['Speed_mode'] = np.nan
 static_data['ROT_mode'] = np.nan
-#static_data['Speed_mode'] = static_data['Speed_mode'].astype(object)
+
+static_data['Speed_mode'] = static_data['Speed_mode'].astype(object)
+static_data['ROT_mode'] = static_data['ROT_mode'].astype(object)
 
 f = lambda x: mode(x, axis=None)[0]
 
@@ -60,3 +61,95 @@ for i in range(len(static_data)):
 #        static_data["Speed_median"].loc[i] = dynamic.iloc[:,3].median()
     except:
         print("couldn't get data on", i+1)
+        
+#%%
+"""
+Pre-processing
+"""   
+simple_dynamic = static_data[static_data.length_from_data_set != -1]
+
+simple_dynamic = simple_dynamic[['length_from_data_set', 'width', 'iwrap_type_from_dataset', 'mean_speed', 'top_speed', 'std_speed', 'Speed_mode', 'Speed_median', 'ROT_mean', 'ROT_max', 'ROT_median', 'ROT_min', 'ROT_mode']]
+    
+#%%    
+"""
+Plotting the new stuff
+"""
+
+import seaborn as sn
+
+#static_data['length/width'] = static_data['length_from_data_set'] / static_data['width']
+#list_of_columns = [9,15,16,17,18,19,21,28,30,22,23,24,26,25,27,31]
+list_of_columns = [9,15,21,28,30,22,23,24,26,25,27,31]
+
+correlation_df = static_data.iloc[:,list_of_columns]
+correlation_df.rename({'length_from_data_set':'vessel_length', 'width':'vessel_width', 'mean_speed':'Speed_mean', 'top_speed':'Speed_max','std_speed':'Speed_std'}, axis = 1, inplace = True)
+
+corrMatrix = correlation_df.corr()
+sn.heatmap(corrMatrix,center = 0, annot=True)
+
+#%%
+import seaborn as sn
+#list_of_CH = [5,6]
+#COG_heading = example_df.iloc[:,list_of_CH]
+
+simple_dynamic = simple_dynamic.fillna(0)
+simple_dynamic_corr = simple_dynamic.drop('iwrap_type_from_dataset', axis=1)
+
+corrMatrix = simple_dynamic_corr.corr()
+sn.heatmap(corrMatrix,center = 0, annot=True)
+
+#%%
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error 
+from sklearn.model_selection import train_test_split 
+from sklearn import preprocessing
+from sklearn import metrics
+
+def evaluation(model, predicted, actual):
+    errors = abs(predicted-actual)
+    averageError = np.mean(abs(predicted-actual))
+    mape = 100 * (errors / actual)
+    accuracy = 100-np.mean(mape)
+    mse = metrics.mean_squared_error(predicted, actual)
+    print(np.mean(mape))
+    print(model, '\nAverage Absolute Error = ', round(averageError),'m', '\nMSE = ', round(mse, 2), '\nAccuracy = ', round(accuracy, 2), '%')
+    return({'averageAbsoluteError': averageError, 'mse':mse, 'accuracy':accuracy})
+
+def LR(output,features, labels):
+    X = features.values#.reshape(-1,1) # features
+    y = labels.values#.reshape(-1,1) # labels
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    regressor = LinearRegression()  
+    regressor.fit(X_train, y_train) #training the algorithm
+    y_pred = regressor.predict(X_test) #predicting
+
+    # visualiation of 10 vessels (predicted vs actual value)
+    lr = np.round(pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()}))
+    lr = lr.head(10)
+    lr.plot(kind='bar',figsize=(10,6))
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    plt.title('Actual vs Predicted {0} (10 vessels), \n Linear Regression'.format(output), fontsize=12)
+    plt.show()
+    #evaluation metrics
+    lrEvaluation = evaluation('Linear Regression {0}'.format(output), y_pred, y_test)
+    return(lrEvaluation, lr)
+
+#%%
+    
+#X = pd.DataFrame(np.c_[static_data['ROT_min'], static_data['mean_speed']], columns=['ROT_min','mean_speed']
+speed_and_rot = simple_dynamic[['mean_speed', 'ROT_min']]
+#speed_and_rot2 = np.c_[simple_dynamic['mean_speed'], simple_dynamic['ROT_min']], columns=['mean_speed','ROT_min']
+lrLength = LR('Length',speed_and_rot, simple_dynamic['length_from_data_set'])
+
+#%%
+
+#simple_dynamic = simple_dynamic['ROT_mode'].dropna(how='all')    
+#X = pd.DataFrame(np.c_[static_data['ROT_min'], static_data['mean_speed']], columns=['ROT_min','mean_speed']
+speed_and_rot = simple_dynamic[['mean_speed', 'top_speed', 'std_speed', 'Speed_mode', 'Speed_median', 'ROT_mean', 'ROT_max', 'ROT_median', 'ROT_min']]
+#speed_and_rot2 = np.c_[simple_dynamic['mean_speed'], simple_dynamic['ROT_min']], columns=['mean_speed','ROT_min']
+lrLength = LR('Length',speed_and_rot, simple_dynamic['length_from_data_set'])
+
+#%%
+simple_dynamic.to_csv(path+'dynamic_for_linear_models.csv', encoding ='utf-8', index=False)
