e85855f57498503398774ad6dd5c54991d31f15d
Maybe real Julia <julia.spicina@gmail.com>
2020-02-12 09:44:51 +0100

    extracted all ROT and speed data from known vessel types, left out unknown and other vessels, left out vessels with -1 length

--- dynamic_data_18_19/6_more_data_merging_dynamic.py
+++ dynamic_data_18_19/6_more_data_merging_dynamic.py
@@ -42,13 +42,19 @@ print('boat is', round(boat['length_from_data_set'].mean(), 1)) #12.8m
 static_data_ships = pd.concat([tanker, cargo, passenger, support, ferry, fishing, boat])
 print(round((len(static_data_ships)/len(static_data_full)*100), 1), 'percent of ships have information about vessel type') #80.4
 
-static_data_ships = static_data_ships.reset_index(drop =True) #reset index
 static_data_ships = static_data_ships[static_data_ships.width != -1] #drop -1 in width
+static_data_ships = static_data_ships.reset_index(drop =True) #reset index
 
 static_data_ships['Speed_mean'] = np.nan
 static_data_ships['Speed_std'] = np.nan
 static_data_ships['Speed_median'] = np.nan
+static_data_ships['Speed_max'] = np.nan
+static_data_ships['Speed_min'] = np.nan
 static_data_ships['Speed_mode'] = np.nan
+
+static_data_ships['ROT_mean'] = np.nan
+static_data_ships['ROT_std'] = np.nan
+static_data_ships['ROT_median'] = np.nan
 static_data_ships['ROT_max'] = np.nan
 static_data_ships['ROT_min'] = np.nan
  #%%
@@ -58,28 +64,34 @@ getting dynamic data for all known ship types
 """
 
 #static_data_ships = static_data_ships[:15] #test
-def get_dynamic(static_data_ships):
+def get_dynamic(static_data_ships_local):
     
-    for i in range(len(static_data_ships)):
+    for i in range(len(static_data_ships_local)):
+        print(i)
         try:
-            print("getting dynamic data for ship number", i+1)
             #getting mmsi name of the ship from the static data 
-            mmsi = static_data.mmsi[i]
+            mmsi = static_data_ships_local.mmsi.iloc[i]
+            print("getting dynamic data for ship ID", mmsi)
+            #print('mmsi ',mmsi)
             #loading the dynamic file for that specific ship
             dynamic = pd.read_csv(dynamic_data_path +"/{}.csv".format(mmsi), sep="\t", index_col=None, error_bad_lines=True)
         except:
-            print("couldn't open file for ship", i+1)
+            print("couldn't open file for ship", mmsi)
             pass
         try:
             #calculating mean, max, std speed for that specific ship and saving it in the static DF
-            dynamic = dynamic[(dynamic[['sog [kn]']] != 0).all(axis=1)] #dropping all 0 values
-            static_data_ships["Speed_mean"].loc[i] = dynamic.iloc[:,3].mean()
-            static_data_ships["Speed_std"].loc[i] = dynamic.iloc[:,3].std()
-            static_data_ships["Speed_median"].loc[i] = dynamic.iloc[:,3].median()
+            #dynamic = dynamic[(dynamic[['sog [kn]']] != 0).all(axis=1)] #dropping all 0 values
+            #TODO NOTE this way of working with pandas, the commented out version is the bad way, the following line is the correct way, 10x faster
+            #static_data_ships["Speed_mean"].loc[i] = dynamic.iloc[:,3].mean()
+            static_data_ships_local.loc[i,"Speed_mean"] = dynamic.iloc[:,3].mean()
+            static_data_ships_local.loc[i,"Speed_std"] = dynamic.iloc[:,3].std()
+            static_data_ships_local.loc[i,"Speed_median"] = dynamic.iloc[:,3].median()
+            static_data_ships_local.loc[i,"Speed_max"] = dynamic.iloc[:,3].max()
+            static_data_ships_local.loc[i,"Speed_min"] = dynamic.iloc[:,3].min()
             dynamic['rounded_speed'] = np.nan
             dynamic['rounded_speed'] = dynamic.iloc[:,3].round(0) #rounding up to integers
         #print(dynamic['rounded_speed'][0])
-            static_data_ships['Speed_mode'].loc[i] = dynamic.mode()['rounded_speed'][0]
+            static_data_ships_local.loc[i,'Speed_mode'] = dynamic.mode()['rounded_speed'][0]
         except Exception as inst:
             print("couldn't get speed data on ship", mmsi)
             print (inst.args)
@@ -87,28 +99,32 @@ def get_dynamic(static_data_ships):
         
         #calculating max and min ROT values
         try:
-            dynamic = dynamic[(dynamic[['rot [deg/min]']] != 0).all(axis=1)] #dropping all 0 values
-            static_data_ships["ROT_max"].loc[i] = dynamic.iloc[:,7].max()
-            static_data_ships["ROT_min"].loc[i] = dynamic.iloc[:,7].min()
+            #dynamic = dynamic[(dynamic[['rot [deg/min]']] != 0).all(axis=1)] #dropping all 0 values
+            static_data_ships_local.loc[i,"ROT_mean"] = dynamic.iloc[:,7].mean()
+            static_data_ships_local.loc[i,"ROT_std"] = dynamic.iloc[:,7].std()
+            static_data_ships_local.loc[i,"ROT_median"] = dynamic.iloc[:,7].median()
+            static_data_ships_local.loc[i,"ROT_max"] = dynamic.iloc[:,7].max()
+            static_data_ships_local.loc[i,"ROT_min"] = dynamic.iloc[:,7].min()
         except:
             print("couldn't get ROT values for ship", mmsi)
-        
             
-    return static_data_ships
+    return static_data_ships_local
         
 #%%       
 import winsound
-frequency = 1000  # Set Frequency To 2500 Hertz
+frequency = 300  # Set Frequency To 2500 Hertz
 duration = 1000  # Set Duration To 1000 ms == 1 second
    
-static_data_ships_1 = static_data_ships[9:11]
+#static_data_ships_1 = static_data_ships[:1000]
+#static_data_ships_1 = static_data_ships_1.reset_index(drop =True) #reset index
 
-dynamic_1 = get_dynamic(static_data_ships_1)
-winsound.Beep(frequency, duration)
+#dynamic_1 = get_dynamic(static_data_ships_1)
+dynamic_all = get_dynamic(static_data_ships)
+#winsound.Beep(frequency, duration)
 
 #%%
-
 #data = static_data_ships.dropna(subset = ['Speed_mean'])       
-dynamic_1_vessel =pd.read_csv(dynamic_data_path + '/245405000.csv', sep="\t", index_col=None, error_bad_lines=False)
+#dynamic_1_vessel =pd.read_csv(dynamic_data_path + '/245405000.csv', sep="\t", index_col=None, error_bad_lines=False)
+#atd_1 = dynamic_1_vessel.iloc[:,3].std()
 
-atd_1 = dynamic_1_vessel.iloc[:,3].std()
+dynamic_all.to_csv(path + 'static_all_with_speed_rot.csv', encoding ='utf-8', index=False)
